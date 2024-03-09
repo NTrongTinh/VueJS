c43ad7dcb6875fe048a89a3cb1283c758eafe7d8
NTrongTinh <trongtinh12b4lh2022@gmail.com>
2024-03-09 14:34:22 +0700

    second commit

---
+++ SHOPCOFFEE-VUE/src/router/index.js
@@ -0,0 +1,41 @@
+import { createWebHistory, createRouter } from "vue-router";
+// import Home from "../views/HomePage.vue";
+// import About from "../views/AboutPage.vue";
+// import Product from "../views/ProductPage.vue";
+// import Cart from "../views/CartPage.vue";
+// import home from "../views/home.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import(/* webpackChunkName: "about" */ '../views/HomePage.vue'),
+    },
+    {
+        path: "/cart",
+        name: "Cart",
+        component: () => import(/* webpackChunkName: "about" */ '../views/CartPage.vue'),
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutPage.vue'),
+    },
+    {
+        path: "/product/:name",   //<-- notice the colon
+        name: "Product",
+        component: () => import(/* webpackChunkName: "about" */ '../views/ProductPage.vue'),
+    },
+    {
+        path: "/productdetails",   //<-- notice the colon
+        name: "Product",
+        component: () => import(/* webpackChunkName: "about" */ '../views/ProductDetails.vue'),
+    },
+];
+
+const router = createRouter({ 
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
